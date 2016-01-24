7d72d1cabf3b00b6e5a27246d6ae8af1d2cdb64c
Hector Juan Soria <hector151293@gmail.com>
2016-01-24 18:51:30 +0000

    Pruebita 6

--- despliegue.php
+++ despliegue.php
@@ -8,7 +8,7 @@ file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  exec('./produccion.sh');
+  shell_exec('git pull');
 }
 
 ?>
