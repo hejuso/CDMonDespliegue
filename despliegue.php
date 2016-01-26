390850b1af67ed06b6a46bd936a9d4eb95a60c6f
Hector Juan Soria <hector151293@gmail.com>
2016-01-26 14:47:03 +0000

    Finalizado con el log

--- despliegue.php
+++ despliegue.php
@@ -1,15 +1,18 @@
 <?php
 
+date_default_timezone_set('Europe/Andorra');
+$date = date('m/d/Y h:i:s a', time());
+
 //$payload = json_decode($_REQUEST['payload']);
 $payload = json_decode(file_get_contents('php://input'));
 
 //log the request
-file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
+file_put_contents('logs/github.txt', print_r("Se ha producido un pull: ".$date."\n", TRUE), FILE_APPEND);
 
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  shell_exec('producion.sh');
+  shell_exec('./produccion.sh');
 }
 
 ?>
