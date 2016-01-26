8367d709279a7e79180f351417ce051210424274
hejuso <hector151293@gmail.com>
2016-01-26 13:09:52 +0100

    Pureba

--- despliegue.php
+++ despliegue.php
@@ -9,7 +9,7 @@ file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  shell_exec('./producion.sh');
+  shell_exec('producion.sh');
 }
 
 ?>
