65ed137af205642b0bedcf8ee77f97fe6d4b7055
Hector Juan Soria <hector151293@gmail.com>
2016-01-26 12:01:56 +0000

    Prueba Casi final

--- despliegue.php
+++ despliegue.php
@@ -1,6 +1,7 @@
 <?php
 
-$payload = json_decode($_REQUEST['payload']);
+//$payload = json_decode($_REQUEST['payload']);
+file_get_contents('php://input');
 
 //log the request
 file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
@@ -8,7 +9,7 @@ file_put_contents('logs/github.txt', print_r($payload, TRUE), FILE_APPEND);
 if ($payload->ref === 'refs/heads/master')
 {
   // path to your site deployment script
-  shell_exec('git pull');
+  shell_exec('./producion.sh');
 }
 
 ?>
